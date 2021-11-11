7cc0ebc7022bd2dbf0df513845a44adf349de879
Rastislav Krutak <xkrutak@fi.muni.cz>
2021-11-11 14:25:12 +0100

    Initial commit

---
+++ src/main/java/com/example/astroapp/AstroAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.astroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstroAppApplication.class, args);
+    }
+
+}
