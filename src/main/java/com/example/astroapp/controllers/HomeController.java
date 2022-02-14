5f32fe079335572c6385d6e5b0730782f0ed1a60
Rastislav Krutak <xkrutak@fi.muni.cz>
2022-02-14 16:38:22 +0100

    added user list to form on the hompeage

--- src/main/java/com/example/astroapp/controllers/HomeController.java
+++ src/main/java/com/example/astroapp/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.astroapp.controllers;
 
+import com.example.astroapp.dao.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @Autowired
+    UserRepo userRepo;
+
     @GetMapping("/")
-    public String displayHome(@AuthenticationPrincipal Model model) {
+    public String displayHome(Model model) {
+        model.addAttribute("users", userRepo.findAll());
         return "home";
     }
 }
