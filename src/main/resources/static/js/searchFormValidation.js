6d849df0d021cb686e7d3367abf5094a7d0ab2a8
Rastislav Krutak <xkrutak@fi.muni.cz>
2022-02-23 15:22:16 +0100

    Coordinates now have to be complete to submit the search form

--- src/main/resources/static/js/searchFormValidation.js
+++ src/main/resources/static/js/searchFormValidation.js
@@ -13,6 +13,25 @@ searchForm.addEventListener('submit', function (event) {
         minInput.classList.add("is-invalid");
         maxInput.classList.add("is-invalid");
     }
+    let raInput = document.getElementById("raInput");
+    let decInput = document.getElementById("decInput");
+    let radiusInput = document.getElementById("radiusInput");
+    // if one of the values is given we need all of them
+    let emptyCoorInputs = [];
+    if (raInput.value === "") {
+        emptyCoorInputs.push(raInput);
+    }
+    if (decInput.value === "") {
+        emptyCoorInputs.push(decInput);
+    }
+    if (radiusInput.value === "") {
+        emptyCoorInputs.push(radiusInput);
+    }
+    if (emptyCoorInputs.length !== 0 && emptyCoorInputs.length !== 3) {
+        event.preventDefault();
+        event.stopPropagation();
+        emptyCoorInputs.map(input => input.classList.add("is-invalid"))
+    }
     let inputs = searchForm.getElementsByTagName("input");
     for (let input of inputs) {
         if (!input.checkValidity()) {
