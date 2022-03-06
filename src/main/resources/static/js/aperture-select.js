38ced292b07a06a80c916a8ce38b38f9b7062419
Rastislav Krutak <xkrutak@fi.muni.cz>
2022-03-06 10:14:44 +0100

    Apertures selects now persist values.

--- src/main/resources/static/js/aperture-select.js
+++ src/main/resources/static/js/aperture-select.js
@@ -1,17 +1,24 @@
+let apertures = new URLSearchParams(window.location.search).get("apertures");
+let apsArray = apertures.split(",");
+for (let i = 0; i < apsArray.length; i++) {
+    document.getElementById(i.toString()).value=apsArray[i];
+}
+
+
+
 function selectAperture() {
-    let searchParams = window.location.search;
+    let searchParams = new URLSearchParams(window.location.search);
+    searchParams.delete("apertures");
     let selects = document.getElementById("apertureSelects")
         .getElementsByTagName("select");
     let selectsArray = Array.from(selects);
     selectsArray.sort((a, b) => a.id - b.id);
     let aperturesParam = "";
-    let selectValues = [];
-    for (let select of selectsArray) {
-        aperturesParam += "&apertures=" + select.value;
-        selectValues.push(select.value);
-    }
-    window.location.replace(searchParams + aperturesParam);
     for (let i = 0; i < selectsArray.length; i++) {
-        selectsArray[i].value=selectValues[i];
+        aperturesParam += selectsArray[i].value;
+        if (i !== selectsArray.length - 1) {
+            aperturesParam += ",";
+        }
     }
+    window.location.replace("object?" + searchParams.toString() + "&apertures="+ aperturesParam);
 }
