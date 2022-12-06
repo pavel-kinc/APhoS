002cb04941ea4a8186c6122c4efefd3a13150dc4
Pavel Kinc <pavelkinc230@gmail.com>
2022-12-06 12:55:02 +0100

    feat(swagger): Added swagger-ui and generated user

---
+++ src/main/java/cz/muni/aphos/openapi/ApiException.java
@@ -0,0 +1,10 @@
+package io.swagger.api;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-12-06T11:18:47.588Z[GMT]")
+public class ApiException extends Exception {
+    private int code;
+    public ApiException (int code, String msg) {
+        super(msg);
+        this.code = code;
+    }
+}
---
+++ src/main/java/cz/muni/aphos/openapi/NotFoundException.java
@@ -0,0 +1,10 @@
+package io.swagger.api;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-12-06T11:18:47.588Z[GMT]")
+public class NotFoundException extends ApiException {
+    private int code;
+    public NotFoundException (int code, String msg) {
+        super(code, msg);
+        this.code = code;
+    }
+}
