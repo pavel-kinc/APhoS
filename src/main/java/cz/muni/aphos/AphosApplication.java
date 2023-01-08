92d0ba183b6985df8e25f19d7e172c54cf388e1a
Pavel Kinc <pavelkinc230@gmail.com>
2023-01-08 23:55:41 +0100

    feat(springdoc): Migrating to springdoc

--- src/main/java/cz/muni/aphos/AphosApplication.java
+++ src/main/java/cz/muni/aphos/AphosApplication.java
@@ -4,14 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 
 /**
  * The entry point of the application.
  */
 @EnableRetry
 @SpringBootApplication
-@EnableOpenApi
 public class AphosApplication {
 
     public static void main(String[] args) {
