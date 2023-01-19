a4f4229bcb3314b0ef7c59302ac0f98bfd95a665
Pavel Kinc <pavelkinc230@gmail.com>
2023-01-20 00:23:52 +0100

    feat(openapi.yml): Generated openapi.yaml replaced old one

--- src/main/java/cz/muni/aphos/config/SecurityConfig.java
+++ src/main/java/cz/muni/aphos/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                     .requestMatchers("/object/**", "/object/download", "/object/aperture").permitAll()
                     .requestMatchers("/js/**", "/css/**", "/js/**","/images/**", "/webjars/**").permitAll()
                     .requestMatchers("/api-docs", "/api/**", "/swagger-ui/**", "/swagger-ui.html", "/swagger-resources/**").permitAll()
-                .requestMatchers("/swagger-ui").permitAll()
+                .requestMatchers("/swagger-ui", "/api-docs.yaml").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .logout()
